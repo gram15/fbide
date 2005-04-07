56276e24cabdcd8bc76d295aa110d30291bfbeb5
vongodric <vongodric>
2005-04-07 16:27:18 +0000

    First pass to FBIde 0.4

---
+++ FBIde0.4_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef FBIDE0_4_PRIVATE_H
+#define FBIDE0_4_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.4.0.1043"
+#define VER_MAJOR	0
+#define VER_MINOR	4
+#define VER_RELEASE	0
+#define VER_BUILD	1043
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.4"
+#define FILE_DESCRIPTION	"IDE for FreeBasic compiler"
+#define INTERNAL_NAME	"fbide"
+#define LEGAL_COPYRIGHT	"Released under GPL"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"fbide.exe"
+#define PRODUCT_NAME	"FBIde"
+#define PRODUCT_VERSION	"0.4"
+
+#endif /*FBIDE0_4_PRIVATE_H*/
