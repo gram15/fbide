0734c0823278eefa57540fc5c4c1b3db35793580
dumbledore3 <dumbledore3>
2005-05-10 05:11:53 +0000

    changed to honor language settings

--- FBIde/FBIde0.4/src/inc/about.h
+++ FBIde/FBIde0.4/src/inc/about.h
@@ -12,10 +12,9 @@
 //[inc]end your include
  
 #include <wx/sizer.h>
-#include <wx/stattext.h>
 #include <wx/statbmp.h>
 #include <wx/button.h>
-#include <wx/timer.h>
+#include <wx/textctrl.h>
 class about;
 class aboutEvt:public wxEvtHandler
 {
@@ -35,27 +34,27 @@ class about:public wxDialog
 {
  friend class aboutEvt;
 public:
- about(wxWindow* parent, wxWindowID id = -1, const wxString& title = wxT("About FBIde"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxSYSTEM_MENU|wxCLOSE_BOX|wxDIALOG_NO_PARENT|wxDEFAULT_DIALOG_STYLE, const wxString& name = wxT("dialogBox"));
+ about(wxWindow* parent, wxWindowID id = -1, const wxString& title = wxT(""), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU|wxCLOSE_BOX|wxDIALOG_NO_PARENT|wxDEFAULT_DIALOG_STYLE|wxMINIMIZE_BOX|wxMAXIMIZE_BOX, const wxString& name = wxT("dialogBox"));
 // Pointer control
- MyFrame *Parent;
- wxStaticText *st4;
  wxStaticBitmap *bm3;
  wxButton *button_ok;
+ wxTextCtrl *txm7;
  virtual ~about();
  virtual void Dabout();
  virtual void initBefore();
  void OnPreCreate();
  void VwXinit();
- void StatChange();
  wxBitmap *bm3Img0;
  wxBitmap fileImgBuf[1];
- wxArrayString textstuffs;
  virtual void initAfter();
+ MyFrame * Parent;
 protected:
  void VwXVwXEvOnButtonClick(wxCommandEvent& event);
  void button_ok_VwXEvOnButtonClick(wxCommandEvent& event,int index=-1);
  DECLARE_EVENT_TABLE()  
 //[win]add your code here
+
+
 
  #define VwX_INIT_OBJECTS_about
 //[win]end your code 
