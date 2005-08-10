37809caa9bf4e6d5c8504b0ba0de759257f07fb9
vongodric <vongodric>
2005-08-10 19:34:19 +0000

    *** empty log message ***

--- FBIde0.4_private.h
+++ FBIde0.4_private.h
@@ -5,19 +5,19 @@
 #define FBIDE0_4_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.4.0.1173"
+#define VER_STRING	"0.4.2.1173"
 #define VER_MAJOR	0
 #define VER_MINOR	4
-#define VER_RELEASE	0
+#define VER_RELEASE	2
 #define VER_BUILD	1173
 #define COMPANY_NAME	""
-#define FILE_VERSION	"0.4"
+#define FILE_VERSION	"0.4.2"
 #define FILE_DESCRIPTION	"IDE for FreeBasic compiler"
 #define INTERNAL_NAME	"fbide"
 #define LEGAL_COPYRIGHT	"Released under GPL"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	"fbide.exe"
 #define PRODUCT_NAME	"FBIde"
-#define PRODUCT_VERSION	"0.4"
+#define PRODUCT_VERSION	"0.4.2"
 
 #endif /*FBIDE0_4_PRIVATE_H*/
