c734c801cc24eaf89319646f71f26cef41eb77a1
vongodric <vongodric>
2005-08-25 21:22:09 +0000

    Cleared up about dialog src

--- src/inc/about.h
+++ src/inc/about.h
@@ -38,48 +38,22 @@
 #include <wx/button.h>
 #include <wx/textctrl.h>
 class about;
-class aboutEvt:public wxEvtHandler
-{
-public:
- aboutEvt(about *parent){ptr_winPan=parent;}
-protected:  
- about *ptr_winPan;
- DECLARE_EVENT_TABLE()     
-//[evt]add your code here
-
-
-
-//[evt]end your code
-};
 
 class about:public wxDialog
 {
- friend class aboutEvt;
-public:
- about(wxWindow* parent, wxWindowID id = -1, const wxString& title = wxT("About FBIde"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxCLOSE_BOX|wxDIALOG_NO_PARENT|wxDEFAULT_DIALOG_STYLE, const wxString& name = wxT("dialogBox"));
-// Pointer control
- wxStaticBitmap *bm3;
- wxButton *button_ok;
- wxTextCtrl *txm7;
- virtual ~about();
- virtual void Dabout();
- virtual void initBefore();
- void OnPreCreate();
- void VwXinit();
- wxBitmap *bm3Img0;
- wxBitmap fileImgBuf[1];
- virtual void initAfter();
- MyFrame * Parent;
-protected:
- void VwXVwXEvOnButtonClick(wxCommandEvent& event);
- void button_ok_VwXEvOnButtonClick(wxCommandEvent& event,int index=-1);
- DECLARE_EVENT_TABLE()  
-//[win]add your code here
-
-
-
- #define VwX_INIT_OBJECTS_about
-//[win]end your code 
+    public:
+        about(wxWindow* parent, wxWindowID id = -1, const wxString& title = wxT("About FBIde"), const wxPoint& pos = wxDefaultPosition, const wxSize& size = wxDefaultSize, long style = wxCAPTION|wxCLOSE_BOX|wxDIALOG_NO_PARENT|wxDEFAULT_DIALOG_STYLE, const wxString& name = wxT("dialogBox"));
+        // Pointer control
+        ~about();
+        wxStaticBitmap *bm3;
+        wxButton *button_ok;
+        wxTextCtrl *txm7;
+        wxBitmap *bm3Img0;
+        wxBitmap fileImgBuf[1];
+        MyFrame * Parent;
+    protected:
+        void On_button_ok(wxCommandEvent& event);
+        DECLARE_EVENT_TABLE()  
 };
 // end about
 
